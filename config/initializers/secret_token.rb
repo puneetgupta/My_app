c52b956f2b45a99d05e77ddaed7d1a41eeea115c
puneet <pgupta@enbake.com>
2013-06-15 13:33:44 +0530

    My first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRepo::Application.config.secret_token = 'd773aae85266c7d42ca272f01ca94c79a33b79d5ad9a9e30cd534f63419e5977165e55eff79bf9fc013c58cc126b578eb49cf58cff1460fd81e12c24bd4a6f67'
